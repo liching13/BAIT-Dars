878e7d993dddc00c6caad0d36098f7919d1b9617
liching13 <dharyll.jan.calaliman013@gmail.com>
2020-03-18 18:22:40 +0800

    Update Exer3

--- Typescript/Dharyll/Exer_3/FormProcess.js
+++ Typescript/Dharyll/Exer_3/FormProcess.js
@@ -1,11 +1,39 @@
-document.getElementById('btnSubmit').addEventListener("click", function () {
-    var fName = document.getElementById("fName").value;
-    var lName = document.getElementById("lName").value;
+/*document.getElementById('btnSubmit').addEventListener("click",function(){
+    let fName:string = (<HTMLInputElement>document.getElementById("fName")).value;
+    let lName:string = (<HTMLInputElement>document.getElementById("lName")).value;
     ProcessData(fName, lName);
 });
-function ProcessData(fname, lname) {
-    var message = 'Congratulation!<br>Account ' + fname + ' ' + lname + ' has been created.';
+
+function ProcessData(fname:string, lname:string):void{
+    let message:string = 'Congratulation!<br>Account ' +fname+ ' ' +lname+ ' has been created.';
+    document.getElementById('txtArea').innerHTML = message;
+
+    (<HTMLInputElement>document.getElementById("fName")).value = '';
+    (<HTMLInputElement>document.getElementById("lName")).value = '';
+}*/
+document.getElementById('btnSubmit').addEventListener("click", ProcessData);
+function ProcessData() {
+    var fName = document.getElementById("fName").value;
+    var lName = document.getElementById("lName").value;
+    var fNum = parseInt(document.getElementById("fNum").value);
+    var sNum = parseInt(document.getElementById("sNum").value);
+    var message = '';
+    if (isNaN(fNum) == true) {
+        fNum = 0;
+    }
+    if (isNaN(sNum) == true) {
+        sNum = 0;
+    }
+    if (fName.length == 0 || lName.length == 0) {
+        message = "Please enter your Full Name!";
+    }
+    else {
+        message = 'Congratulation!<br>Account ' + fName + ' ' + lName + ' has been created.';
+        message += "<br>" + fNum.toString() + " + " + sNum.toString() + " = " + (fNum + sNum).toString();
+    }
     document.getElementById('txtArea').innerHTML = message;
     document.getElementById("fName").value = '';
     document.getElementById("lName").value = '';
+    document.getElementById("fNum").value = '';
+    document.getElementById("sNum").value = '';
 }
--- Typescript/Dharyll/Exer_3/FormProcess.ts
+++ Typescript/Dharyll/Exer_3/FormProcess.ts
@@ -17,9 +17,23 @@ document.getElementById('btnSubmit').addEventListener("click",ProcessData);
 function ProcessData(){
     let fName:string = (<HTMLInputElement>document.getElementById("fName")).value;
     let lName:string = (<HTMLInputElement>document.getElementById("lName")).value;
-    let message:string = 'Congratulation!<br>Account ' +fName+ ' ' +lName+ ' has been created.';
-    document.getElementById('txtArea').innerHTML = message;
+    let fNum:number = parseInt((<HTMLInputElement>document.getElementById("fNum")).value);
+    let sNum:number = parseInt((<HTMLInputElement>document.getElementById("sNum")).value);
+    let message:string = '';
 
-    (<HTMLInputElement>document.getElementById("fName")).value = '';
-    (<HTMLInputElement>document.getElementById("lName")).value = '';
+    if(isNaN(fNum) == true){fNum = 0;}
+    if(isNaN(sNum) == true){sNum = 0;}
+
+    if(fName.length == 0 || lName.length == 0){
+        message = "Please enter your Full Name!";
+    }
+    else{
+        message = 'Congratulation!<br>Account ' +fName+ ' ' +lName+ ' has been created.';
+        message += "<br>"+ fNum.toString()+ " + "+ sNum.toString()+ " = "+(fNum +sNum).toString();
+    }
+        document.getElementById('txtArea').innerHTML = message;
+        (<HTMLInputElement>document.getElementById("fName")).value = '';
+        (<HTMLInputElement>document.getElementById("lName")).value = '';
+        (<HTMLInputElement>document.getElementById("fNum")).value = '';
+        (<HTMLInputElement>document.getElementById("sNum")).value = ''; 
 }
