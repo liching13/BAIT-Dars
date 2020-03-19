7277f0b592a5446a5c601e38166e59e22bac8b32
liching13 <dharyll.jan.calaliman013@gmail.com>
2020-03-19 14:10:42 +0800

    Add C# Code Challenge

---
+++ C#/Dharyll/Codes/KangarooCodingChallenge8/KangarooCodingChallenge8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KangarooCodingChallenge8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "repeat";
+            do
+            {
+                Console.WriteLine("Enter two(2) Kangaroo Location and Speed followed by the number of jumps they should do");
+                Console.WriteLine("(Ex: 0 3 4 2 : K1 = 0, S1 = 3, K2 = 4, S2 = 2) \n(Ex: 4 : Number of Jumps = 4)");
+                Console.WriteLine("Enter here:");
+                result = GetJump(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries), int.Parse(Console.ReadLine()));
+            } while (result == "repeat");
+
+            Console.WriteLine($"Will the kangaroo meet? {result}");
+        }
+        public static string GetJump(string[] kangarooJump, int jumpDistance)
+        {
+            string jump = "repeat";
+            int fKang = int.Parse(kangarooJump[0]);
+            int fKangJump = int.Parse(kangarooJump[1]);
+            int sKang = int.Parse(kangarooJump[2]);
+            int sKangJump = int.Parse(kangarooJump[3]);
+
+            if((0 <= fKang && fKang < sKang && sKang <= 10000) && (0 < fKangJump && fKangJump <= 10000) && (0 < sKangJump && sKangJump < 10000) && (0 < jumpDistance && jumpDistance < 10000))
+            {
+                for(int i = 0; i < jumpDistance; i++)
+                {
+                    fKang += fKangJump;
+                    sKang += sKangJump;
+
+                    if (fKang == sKang) { jump = "Yes"; break; }
+                    else { jump = "No"; }
+                }
+            }
+            return jump;
+        }
+    }
+
+    
+}
---
+++ C#/Dharyll/Codes/TrainCodingChallenge9/TrainCodingChallenge9/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TrainCodingChallenge9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a series of number (Ex: 5 4 3 2 1):");
+                int[] sortedNum = TrainSortOdd(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries));
+                foreach(int i in sortedNum) { Console.Write($"{i}, "); }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+
+        public static int[] TrainSortOdd(string[] numbers)
+        {
+            List<int> numOdd = new List<int>();
+            List<int> numIndex = new List<int>();
+            int[] num = new int[numbers.Length];
+
+            //change data type of string array to int array
+            for(int i = 0; i <numbers.Length; i++)
+            {
+                num[i] = int.Parse(numbers[i]);
+            }
+
+            //gets the odd numbers and there index
+            for(int i = 0; i < num.Length; i++)
+            {
+                if(num[i] != 0 && num[i]%2 != 0) 
+                {
+                    numOdd.Add(num[i]);
+                    numIndex.Add(i);
+                }
+            }
+
+            //sort all odd numbers
+            numOdd.Sort();
+
+            //insert the sorted odd numbers back to the array
+            for(int i = 0; i < numIndex.Count; i++)
+            {
+                num[numIndex.ElementAt(i)] = numOdd.ElementAt(i);
+            }
+
+            return num;
+        }
+    }
+}
