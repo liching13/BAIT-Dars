d9605027338266793a518d716b0d52f1caa7d294
liching13 <dharyll.jan.calaliman013@gmail.com>
2020-03-19 14:56:04 +0800

    PR jayson's c#

---
+++ C#/Jayson/6 kyu/sortodd/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+namespace sortodd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[] {5, 3, 2, 8, 1, 4};
+            for(int i =0;i<input.Length;i++)
+            {
+                Console.WriteLine(input[i]);
+            }
+            SortArray(input);
+            Console.WriteLine(" ");
+               for(int i =0;i<input.Length;i++)
+            {
+                Console.WriteLine(input[i]);
+            }
+        }
+        public static int[] SortArray(int[] array)
+        {
+            int oddcount=0;
+            int temp=0;
+            for(int i=0;i<array.Length;i++)
+            {
+                if(array[i]%2!=0)
+                    oddcount++;
+            }
+            int[] oddarray = new int[oddcount];
+            
+            for(int i =0; i<array.Length;i++){
+                if(array[i]%2!=0)
+                {
+                    oddarray[temp]=array[i];
+                    temp++;
+                 }
+            }
+            Array.Sort(oddarray);
+            temp = 0;
+            for(int i=0;i<array.Length;i++){
+                if(array[i]%2==1){
+                    array[i]=oddarray[temp];
+                    temp++;
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ C#/Jayson/kangaroo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace kangaroo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(kangaroo(0,3,4,4));
+        }
+        static string kangaroo(int x1, int v1, int x2, int v2)
+        {
+        return v1>v2&&(x2-x1)%(v1-v2)==0?"YES":"NO";
+        }
+    }
+}
